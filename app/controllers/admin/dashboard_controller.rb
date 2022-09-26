6edea247a3d0fa3e756c5d7b9bf430d2b7738061
Sarah Moss <sarah-moss@live.com>
2022-09-26 19:27:40 +0000

    Pull in product + category totals to admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < ApplicationController
   def show
+    # Display a count of how many products are in the database
+    @productsTotal = Product.count
+    # Display a count of how many categories are in the database
+    @categoriesTotal = Category.count
   end
 end
