781d6d97c9a9cb9d02d73f6629a9f633135ad2a9
Sarah Moss <sarah-moss@live.com>
2022-09-28 05:22:34 +0000

    Fix name error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,20 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
   end
   
   def create
-    @user = User.new(params[:user])
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to '/', notice: "Woop, you are now signed up!"
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/'
     else
       redirect_to '/signup'
     end
   end
 
-    private
-
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
