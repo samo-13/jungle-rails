0b770bf6b3aba8764dc61ab2b6b289f5a65ff87a
Sarah Moss <sarah-moss@live.com>
2022-09-28 03:45:48 +0000

    Add has_secure_password to User model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
   def new
   end
+  
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: "You are logged in."
+    else
+      flash.now.alert = "Oops, your email or password is invalid"
+      render "new"
+    end
+  end
+  
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: "You are logged out."
+  end
 end
